fa3ebb0905ae643bba1bbe01ad0b5440fa2dfcce
krogers <keith.rogers@loveholidays.com>
2024-12-11 13:25:06 +0000

    Format

--- typescript/2024/day11/day11.ts
+++ typescript/2024/day11/day11.ts
@@ -1,62 +1,62 @@
-import { readFile } from 'node:fs/promises';
+import { readFile } from "node:fs/promises";
 
 const readInput = async (fileName: string): Promise<number[]> => {
-  const data = await readFile(fileName);
-  return data.toString().split(' ').map(Number);
-}
-
-const blinkValue = (value: number): number[] => {
-  if (value === 0) {
-    return [1];
-  } else {
-    const stringValue = String(value);
-    if (stringValue.length % 2 === 0) {
-      return [
-        Number(stringValue.substring(0, stringValue.length / 2)),
-        Number(stringValue.substring(stringValue.length / 2)),
-      ];
-    } else {
-      return [ value * 2024];
-    }
-  }
-}
-
-const cache: Record<number, Record<number, number>> = {};
-
-const getValueLength = (value: number, depth: number): number => {
-  if (depth === 0) {
-    return 1;
-  } else {
-    if (cache[depth]?.[value]) {
-      return cache[depth][value];
-    }
-
-    const pv = blinkValue(value);
-    let total = 0;
-    for (let i = 0; i < pv.length; i++) {
-      total += getValueLength(pv[i], depth - 1);
-    }
-
-    if (cache[depth] === undefined) {
-      cache[depth] = {};
-    }
-
-    cache[depth][value] = total;
-
-    return total;
-  }
-}
-
-const process = (data: number[]): number => {
-  let total = 0;
-
-  for (let n = 0; n < data.length; n++) {
-    total += getValueLength(data[n], 75);
-  }
-
-  return total;
+	const data = await readFile(fileName);
+	return data.toString().split(" ").map(Number);
 };
 
-readInput('input.txt')
-  .then(process)
-  .then(console.log);
+const blinkValue = (value: number): number[] => {
+	if (value === 0) {
+		return [1];
+	} else {
+		const stringValue = String(value);
+		if (stringValue.length % 2 === 0) {
+			return [
+				Number(stringValue.substring(0, stringValue.length / 2)),
+				Number(stringValue.substring(stringValue.length / 2)),
+			];
+		} else {
+			return [value * 2024];
+		}
+	}
+};
+
+const cache: Record<string, number> = {};
+
+const getValueLength = (value: number, depth: number): number => {
+	if (depth === 0) {
+		return 1;
+	} else {
+		const key = [value, depth].join(",");
+		if (cache[key]) {
+			return cache[key];
+		}
+
+		const pv = blinkValue(value);
+		let total = 0;
+		for (let i = 0; i < pv.length; i++) {
+			total += getValueLength(pv[i], depth - 1);
+		}
+
+		cache[key] = total;
+
+		return total;
+	}
+};
+
+const process =
+	(depth: number) =>
+	(data: number[]): number => {
+		let total = 0;
+
+		for (let n = 0; n < data.length; n++) {
+			total += getValueLength(data[n], depth);
+		}
+
+		return total;
+	};
+
+readInput("input.txt")
+	.then(process(75))
+	.then(console.log)
+	.then(() => console.log("cache size", Object.keys(cache).length));
